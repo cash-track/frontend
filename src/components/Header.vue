2d879f3effe47fb1c0ecb8293ffac17c2a3663e2
Volodymyr Komarov <vovikems@gmail.com>
2020-07-04 16:14:48 +0300

    Implement UI for password forgot and reset form. Fix route links

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,15 +8,15 @@
 
                 <b-collapse id="nav-collapse" is-nav>
                     <b-navbar-nav>
-                        <b-nav-item :to="{path: '/'}" exact-active-class="active">Home</b-nav-item>
-                        <b-nav-item to="help" exact-active-class="active">Help</b-nav-item>
-                        <b-nav-item to="about" exact-active-class="active">About</b-nav-item>
+                        <b-nav-item :to="{name: 'home'}" exact-active-class="active">Home</b-nav-item>
+                        <b-nav-item :to="{name: 'help'}" exact-active-class="active">Help</b-nav-item>
+                        <b-nav-item :to="{name: 'about'}" exact-active-class="active">About</b-nav-item>
                     </b-navbar-nav>
 
                     <b-navbar-nav class="ml-auto">
                         <b-navbar-nav>
-                            <b-nav-item to="login" exact-active-class="active">Sign In</b-nav-item>
-                            <b-nav-item to="register" exact-active-class="active">Sign Up</b-nav-item>
+                            <b-nav-item :to="{name: 'login'}" exact-active-class="active">Sign In</b-nav-item>
+                            <b-nav-item :to="{name: 'register'}" exact-active-class="active">Sign Up</b-nav-item>
                         </b-navbar-nav>
                     </b-navbar-nav>
                 </b-collapse>
@@ -26,7 +26,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Header extends Vue {
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,20 +1,22 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
-import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
-import Register from '../views/Register.vue'
+import Home from '@/views/Home.vue'
+import Login from '@/views/Login.vue'
+import Register from '@/views/Register.vue'
+import ForgotPassword from "@/views/ForgotPassword.vue";
+import ResetPassword from "@/views/ResetPassword.vue";
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home,
   },
   {
     path: '/about',
-    name: 'About',
+    name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -22,19 +24,29 @@ const routes: Array<RouteConfig> = [
   },
   {
     path: '/help',
-    name: 'Help',
+    name: 'help',
     component: () => import(/* webpackChunkName: "help" */ '../views/Help.vue')
   },
   {
     path: '/login',
-    name: 'Login',
+    name: 'login',
     component: Login,
   },
   {
     path: '/register',
-    name: 'Register',
+    name: 'register',
     component: Register,
   },
+  {
+    path: '/password/forgot',
+    name: 'forgot-password',
+    component: ForgotPassword,
+  },
+  {
+    path: '/password/reset/:code',
+    name: 'reset-password',
+    component: ResetPassword,
+  }
 ]
 
 const router = new VueRouter({
