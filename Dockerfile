566306c25e08dee92da58c2b9c7c4241cf0af291
Volodymyr Komarov <vovikems@gmail.com>
2021-01-18 23:48:31 +0200

    Minor fixes for deployment

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,8 @@ FROM nginx:stable-alpine as production-stage
 
 COPY --from=build-stage /app/dist /usr/share/nginx/html
 
+COPY nginx.conf /etc/nginx/nginx.conf
+
 COPY entrypoint.sh /usr/share/nginx/
 
 ENTRYPOINT ["/usr/share/nginx/entrypoint.sh"]
--- src/components/profile/ProfileTitle.vue
+++ src/components/profile/ProfileTitle.vue
@@ -1,5 +1,6 @@
 <template>
     <div>
+        <profile-avatar :user="user" size="lg"></profile-avatar>
         <h4 class="pt-3">{{ firstName }} {{ lastName }} (@{{ nickName }})</h4>
         <h6>
             <a :href="`mailto:${email}`">{{ email }}</a>
@@ -13,9 +14,16 @@
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
-
-@Component
+import ProfileAvatar from "@/components/profile/ProfileAvatar.vue";
+import {UserInterface} from "@/api/users";
+@Component({
+    components: {ProfileAvatar}
+})
 export default class ProfileTitle extends Vue {
+    get user(): UserInterface {
+        return this.$store.state.profile
+    }
+
     get firstName(): string {
         return this.$store.state.profile.name
     }
