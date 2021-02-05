96ab7832f6d4dc55ff230da6a97b3674dcf0f84b
Volodymyr Komarov <vovikems@gmail.com>
2021-02-05 12:16:19 +0200

    Fix responsibility of wallet title and details section

--- src/views/WalletView.vue
+++ src/views/WalletView.vue
@@ -43,24 +43,24 @@
             </div>
 
             <div class="wallet-details d-flex justify-content-between">
-                <span class="text-left">
+                <span class="text-center text-sm-left">
                     <span class="text-muted">Income</span><br>
-                    <span class="text-success">
-                        <b-icon-arrow-up variant="success" scale="1.5"></b-icon-arrow-up>
+                    <span class="text-success wallet-total">
+                        <b-icon-arrow-up variant="success" scale="1.5" class="d-none d-sm-inline"></b-icon-arrow-up>
                         {{ walletTotal.totalIncomeAmount | money(wallet.defaultCurrency) }}
                     </span>
                 </span>
                 <span class="text-center">
                     <span class="text-muted">Available</span><br>
-                    <span class="text-info">
+                    <span class="text-info wallet-total">
                         {{ walletTotal.totalAmount | money(wallet.defaultCurrency) }}
                     </span>
                 </span>
-                <span class="text-right">
+                <span class="text-center text-sm-right">
                     <span class="text-muted">Expense</span><br>
-                    <span class="text-danger">
+                    <span class="text-danger wallet-total">
                         {{ walletTotal.totalExpenseAmount | money(wallet.defaultCurrency) }}
-                        <b-icon-arrow-down variant="danger" scale="1.5"></b-icon-arrow-down>
+                        <b-icon-arrow-down variant="danger" scale="1.5" class="d-none d-sm-inline"></b-icon-arrow-down>
                     </span>
                 </span>
             </div>
@@ -223,6 +223,10 @@ export default class WalletView extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import "node_modules/bootstrap/scss/functions";
+@import "node_modules/bootstrap/scss/variables";
+@import "node_modules/bootstrap/scss/mixins/_breakpoints";
+
 h3 .badge {
     font-size: 12px;
     font-weight: 400;
@@ -250,6 +254,18 @@ h3 .badge {
     border-top: 1px solid #eee;
     border-bottom: 1px solid #eee;
 
+    &>:nth-child(1) {
+        order: 1;
+    }
+
+    &>:nth-child(2) {
+        order: 2;
+    }
+
+    &>:nth-child(3) {
+        order: 3;
+    }
+
     .text-muted {
         font-size: 12px;
     }
@@ -257,6 +273,48 @@ h3 .badge {
     .text-success, .text-danger, .text-info {
         font-size: 40px;
         line-height: 40px;
+    }
+
+    .wallet-total {
+        white-space: nowrap;
+    }
+}
+
+@include media-breakpoint-down(sm) {
+    .wallet-header {
+        flex-wrap: wrap;
+    }
+
+    .wallet-details {
+        flex-wrap: wrap;
+
+        &>* {
+            display: block;
+        }
+
+        &>:nth-child(1) {
+            width: 50%;
+            order: 1;
+        }
+
+        &>:nth-child(2) {
+            width: 100%;
+            order: 3;
+        }
+
+        &>:nth-child(3) {
+            width: 50%;
+            order: 2;
+        }
+    }
+}
+
+@include media-breakpoint-down(xs) {
+    .wallet-details {
+        &>:nth-child(1), &>:nth-child(2), &>:nth-child(3) {
+            width: 100%;
+            padding: 10px 0;
+        }
     }
 }
 </style>
