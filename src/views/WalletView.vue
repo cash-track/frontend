5c7be46948fcec494de2d9c6d92a7224c34924e2
Volodymyr Komarov <vovikems@gmail.com>
2022-06-19 21:14:59 +0300

    Hide wallet tags box if no tags used

--- src/views/WalletView.vue
+++ src/views/WalletView.vue
@@ -76,7 +76,7 @@
                 </span>
             </div>
 
-            <div class="wallet-tags">
+            <div class="wallet-tags" v-show="tags.length">
                 <tag v-for="tag of tags"
                      :tag="tag"
                      :key="tag.id"
