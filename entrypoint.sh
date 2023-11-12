6ddd3fd1bbcd915cfab6be9c73a31d5f54789485
Volodymyr Komarov <vovikems@gmail.com>
2023-11-12 21:17:59 +0200

    Fix gateway URL resolution

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 JSON_STRING='window.configs = { \
   "VUE_APP_BASE_URL":"'"${VUE_APP_BASE_URL}"'", \
   "VUE_APP_API_URL":"'"${VUE_APP_API_URL}"'", \
+  "VUE_APP_GATEWAY_URL":"'"${VUE_APP_GATEWAY_URL}"'", \
   "VUE_APP_WEBSITE_URL":"'"${VUE_APP_WEBSITE_URL}"'" \
 }'
 
